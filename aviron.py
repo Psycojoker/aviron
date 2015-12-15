2b3001fd8cc28f77a53b8b5ad9338cbf21d8318d
Laurent Peuch <cortex@worlddomination.be>
2015-12-15 11:54:46 +0100

    [enh] start to implement a cursor

--- aviron.py
+++ aviron.py
@@ -39,9 +39,12 @@ class RenderingBuffer(object):
     def __init__(self, red):
         self.red = red
         self.buffer = red.dumps()
+        self.cursor = Cursor(red)
 
     def get_window(self, size):
         height, width = size
+
+        x1, y1, x2, y2 = self.cursor.get_cursor_square()
 
         to_return = []
 
@@ -56,6 +59,16 @@ class RenderingBuffer(object):
                 to_return.append((i[:width - 1] + "\n", COLORS["default"]))
 
         return to_return
+
+
+class Cursor(object):
+    def __init__(self, red):
+        self.red = red
+        self.current = red[0]
+        self.bounding_box = red[0].absolute_bounding_box
+
+    def get_cursor_square(self):
+        return map(lambda x: x - 1, self.bounding_box.top_left), map(lambda x: x - 1, self.bounding_box.bottom_right)
 
 
 def main(ncurse_window):
