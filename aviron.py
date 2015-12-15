ee2e5948506f8cdae001a2e508cd438c88b4586e
Laurent Peuch <cortex@worlddomination.be>
2015-12-15 10:34:59 +0100

    [enh] handle terminal resizing \o/

--- aviron.py
+++ aviron.py
@@ -4,17 +4,36 @@ from redbaron import RedBaron
 
 class Window(object):
     def __init__(self, ncurse_window, red):
-        self.red = red
+        self.rendering_buffer = RenderingBuffer(red)
         self.ncurse_window = ncurse_window
 
     def render(self):
-        self.ncurse_window.addstr(0, 0, self.red.dumps())
+        self.ncurse_window.addstr(0, 0, self.rendering_buffer.get_window(size=self.ncurse_window.getmaxyx()))
         self.ncurse_window.refresh()
 
     def loop(self):
         while 42:
             self.render()
             self.ncurse_window.getch()
+
+
+class RenderingBuffer(object):
+    def __init__(self, red):
+        self.red = red
+        self.buffer = red.dumps()
+
+    def get_window(self, size):
+        height, width = size
+
+        to_return = []
+
+        for line_number, i in enumerate(self.buffer.split("\n")):
+            if line_number >= height:
+                break
+
+            to_return.append(i[:width - 1])
+
+        return "\n".join(to_return)
 
 
 def main(ncurse_window):
